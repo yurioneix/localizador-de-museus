c9aabdab57a70e17d836f9c73fc56e5643afbe96
yurioneix <yurioneix18@gmail.com>
2023-09-04 16:49:58 -0300

    Implementa classe Museum

--- src/main/java/com/betrybe/museumfinder/model/Museum.java
+++ src/main/java/com/betrybe/museumfinder/model/Museum.java
@@ -4,5 +4,85 @@ package com.betrybe.museumfinder.model;
  * Classe Museum.
  */
 public class Museum {
+  Long id;
+  String name;
+  String description;
+  String address;
+  String collectionType;
+  String subject;
+  String url;
+  Coordinate coordinate;
+  Long legacyId;
 
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddres(String addres) {
+    this.address = addres;
+  }
+
+  public String getCollectionType() {
+    return collectionType;
+  }
+
+  public void setCollectionType(String collectionType) {
+    this.collectionType = collectionType;
+  }
+
+  public String getSubject() {
+    return subject;
+  }
+
+  public void setSubject(String subject) {
+    this.subject = subject;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public Coordinate getCoordinate() {
+    return coordinate;
+  }
+
+  public void setCoordinate(Coordinate coordinate) {
+    this.coordinate = coordinate;
+  }
+
+  public Long getLegacyId() {
+    return legacyId;
+  }
+
+  public void setLegacyId(Long legacyId) {
+    this.legacyId = legacyId;
+  }
 }
